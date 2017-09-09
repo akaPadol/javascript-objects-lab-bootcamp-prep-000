cb94ff9f7b07be10e2a14cc351862fd1bc285d1c
Antonino Badalamenti <bada.nino95@gmail.com>
2017-09-09 04:50:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function updateObjectWithKeyAndValue(obj,key,value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(obj,key,value){
-  obj=updateObjectWithKeyAndValue(obj,key,value)
+  obj[key]=value
   return obj
 }
 
