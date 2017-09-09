ca7ed41c59ffce6c8ffe002f066e407e3aa20282
Antonino Badalamenti <bada.nino95@gmail.com>
2017-09-09 03:38:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var recipes = new Object({})
+
+ console.log(updateObjectWithKeyAndValue(recipes,"lol",123))
 function updateObjectWithKeyAndValue(obj,key,value){
   var ob = new Object({})
   ob[key]=value
